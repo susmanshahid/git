ed1cfbe38e252ebdb9d9dd6c1b948a63507e7460
susmanshahid <usmanmad@gmail.com>
2021-08-03 14:16:36 +0530

    done

--- src/org/hex/conflict.java
+++ src/org/hex/conflict.java
@@ -1,5 +1,14 @@
 package org.hex;
 
 public class conflict {
+	public static void username() {
+		
 
+	}{
+System.out.println("login1");
+	
+}
+	public static void main(String[] args) {
+		username();
+	}
 }

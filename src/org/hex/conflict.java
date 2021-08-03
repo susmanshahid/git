e49a936ddccb203cf1195f1291922ccdd9a32361
susmanshahid <usmanmad@gmail.com>
2021-08-03 14:08:20 +0530

    password functionality

--- src/org/hex/conflict.java
+++ src/org/hex/conflict.java
@@ -1,5 +1,14 @@
 package org.hex;
 
 public class conflict {
+	public static void password() {
+		
 
+	} {
+		System.out.println("logout");
+
+	}
+ public static void main(String[] args) {
+	password();
+}
 }
